2da7b1e80185d4da66d4d6b44d0e813714a4f916
Giancarlo Galliani Pecchia <giancarlo.pecchia@arcadiagroup.co.uk>
2017-12-25 13:23:03 -0500

    Configure request for access token.

--- handler.js
+++ handler.js
@@ -1,50 +1,56 @@
-'use strict'
+require('dotenv').config()
 const fetch = require('node-fetch')
+const request = require('superagent')
 
 module.exports.getToken = (event, context, callback) => {
-
   const config = {
-    "clientId" : process.env.CLIENT_ID,
-    "clientSecret" : process.env.CLIENT_SECRET
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
   }
 
   const makeAuthHeader = (config) => {
-    const b64 = new Buffer(`${config.clientId}:${config.clientSecret}`).toString('base64')
-    return {
-      Authorization: `Basic ${b64}`,
-      'Access-Control-Allow-Origin': '*'
-    }
+    const buffer = new Buffer(`${config.clientId}:${config.clientSecret}`).toString('base64')
+    return `Basic ${buffer}`
   }
 
-  const SpotifyAuth = () => new Promise((resolve, reject) => {
-    fetch('https://accounts.spotify.com/api/token', {
-      method: 'post',
-      headers: makeAuthHeader(config),
-      credentials: 'include',
-      body: {
-        grant_type: 'client_credentials'
-      }
-    }).then((res) => {
-      console.log(res)
-      return {
-        access_token: res.text()
-      }
+  const SpotifyAuth = () =>
+    new Promise((resolve, reject) => {
+      const headers = makeAuthHeader(config)
+      const body = { grant_type: 'client_credentials' }
+      return (
+        request
+          .post('https://accounts.spotify.com/api/token')
+          // .type('form')
+          .set('Authorization', makeAuthHeader(config))
+          .send(body)
+          .then((res, req) =>
+            resolve({
+              access_token: res.body.access_token
+            })
+          )
+          .catch((err) => reject(err))
+      )
     })
-  })
 
-  SpotifyAuth().then(res => {
-
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: 'Got Spotify token.',
-        input: res.text()
-      })
-    }
-    callback(null, response)
-  })
-
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+  SpotifyAuth()
+    .then((res) => {
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: 'Got Spotify token.',
+          input: res
+        })
+      }
+      return callback(null, response)
+    })
+    .catch((err) => {
+      const response = {
+        statusCode: 400,
+        body: JSON.stringify({
+          message: 'Error obtaining token',
+          input: err
+        })
+      }
+      return callback(response)
+    })
 }
